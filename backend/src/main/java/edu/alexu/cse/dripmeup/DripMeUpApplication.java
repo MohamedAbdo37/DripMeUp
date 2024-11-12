83424d792dd98f41c35014bcaf0db9a05d109112
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-11-12 11:04:26 +0200

    Build the Project Directory Structure

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/DripMeUpApplication.java
@@ -0,0 +1,13 @@
+package edu.alexu.cse.dripmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripMeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DripMeUpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/alexu/cse/dripmeup/DripMeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alexu.cse.dripmeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripMeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
