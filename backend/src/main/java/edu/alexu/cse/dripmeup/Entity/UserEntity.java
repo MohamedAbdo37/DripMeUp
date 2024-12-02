1b85dfc0864981c518583edc559381141b229ada
alihassann191 <ah2070643@gmail.com>
2024-12-02 19:44:35 +0200

    UserEntity class

--- backend/src/main/java/edu/alexu/cse/dripmeup/Entity/UserEntity.java
+++ backend/src/main/java/edu/alexu/cse/dripmeup/Entity/UserEntity.java
@@ -1,19 +1,53 @@
 package edu.alexu.cse.dripmeup.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
-@Table(name = "USER")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserEntity {
+@Table(name = "User")
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class UserEntity {
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "photo")
+    @Lob
+    private byte[] photo;
+    public String getUserName() {
+        return this.userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public byte[] getPhoto() {
+        return this.photo;
+    }
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
 
 }
