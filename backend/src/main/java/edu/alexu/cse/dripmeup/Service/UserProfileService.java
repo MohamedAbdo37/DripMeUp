549452344f865d763920648a69a6524978ac1aca
PierreJack <pierrejacques2002@gmail.com>
2024-12-03 23:39:46 +0200

    Create UserProfileService.java

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/Service/UserProfileService.java
@@ -0,0 +1,31 @@
+package edu.alexu.cse.dripmeup.Service;
+
+import edu.alexu.cse.dripmeup.Entity.Person;
+import edu.alexu.cse.dripmeup.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class UserProfileService {
+    UserRepository userRepository;
+    @Autowired
+    public UserProfileService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public Person getUserProfile(String id) {
+        return new Person();
+    }
+    public byte[] getUserProfilePhoto(String id) {
+        return new byte[0];
+    }
+    public String changeUserProfilePhoto(String id, byte[] photo) {
+        return "POST request received with body: ";
+    }
+
+    public String changeUsername(String id, String name) {
+        return "POST request received with body: ";
+    }
+}
