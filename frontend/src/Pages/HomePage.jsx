88652271f2a0b631abdfc541ff1d36b1d06978ab
zoaa3054 <zoaa3054@gmail.com>
2024-12-04 03:28:17 +0200

    Admin login page added

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -31,10 +31,10 @@ const HomePage = () =>{
             <img src={user.picture} alt='' style={{borderRadius:'50%',  width: '10rem', height: '10rem'}}></img>
             <p>Email: {user.email}</p>
             <p>Username: {user.userName}</p>
-            <p>Phone: {user.phone}</p>
+            {/* <p>Phone: {user.phone}</p>
             <p>Country: {user.country}</p>
             <p>City: {user.city}</p>
-            <p>Address: {user.address}</p>
+            <p>Address: {user.address}</p> */}
             <p>Gender: {user.gender}</p>
         </>);
     }
