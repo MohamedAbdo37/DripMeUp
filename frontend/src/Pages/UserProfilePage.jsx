1730efd7ee53e4c84ed18ad0c316b2c16a49f52f
PierreJack <pierrejacques2002@gmail.com>
2024-12-04 02:09:20 +0200

    Added UserProfile frontend

---
+++ frontend/src/Pages/UserProfilePage.jsx
@@ -0,0 +1,11 @@
+import UserProfileBox from "../Components/UserProfileBox";
+
+const ProfilePage = () => {
+    return (
+        <div className="profilePage">
+            <UserProfileBox />
+        </div>
+    );
+};
+
+export default ProfilePage;
