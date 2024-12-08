290d1846ae430382de5e2d1b7670d311b6779343
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-12-09 00:28:26 +0200

    add adminSignUp API

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/Controller/AdminSessionController.java
@@ -0,0 +1,35 @@
+package edu.alexu.cse.dripmeup.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.alexu.cse.dripmeup.Entity.Person;
+import edu.alexu.cse.dripmeup.Repository.AdminRepository;
+import edu.alexu.cse.dripmeup.component.SessionManager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080/api/6/")
+@RequestMapping("admin")
+public class AdminSessionController {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    
+    private SessionManager sessionManager = new SessionManager(adminRepository);
+
+    @GetMapping("signUp")
+    public ResponseEntity<Person> getMethodName(@RequestHeader("UserName") String userName, @RequestHeader("Password") String password) {
+        Person person = sessionManager.adminSignUP(userName, password);
+        return ResponseEntity.ok(person);
+    }
+    
+    
+}
