5ee2b77d1db9a5d1de5fc067189151c8467cfce3
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-12-13 23:45:11 +0200

    CodeEntity & CodeRepository

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/Entity/CodeEntity.java
@@ -0,0 +1,39 @@
+package edu.alexu.cse.dripmeup.Entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "VerificationCode")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeEntity implements EntityIF{
+    
+    @Id
+    @Column(name = "codeId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codeID;
+
+    @Column(name = "code")
+    private int code;
+
+    @Column(name = "email")
+    private String email;
+
+    // @Column(name = "createdDate")
+    // private Date createdDate;
+
+    
+}
---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/Repository/CodeRepository.java
@@ -0,0 +1,8 @@
+package edu.alexu.cse.dripmeup.Repository;
+
+import edu.alexu.cse.dripmeup.Entity.CodeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CodeRepository extends JpaRepository<CodeEntity, Long> {
+    CodeEntity findByCodeID(Long codeID);
+}
