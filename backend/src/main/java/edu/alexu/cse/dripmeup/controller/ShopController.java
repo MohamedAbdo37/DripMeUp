ff0780b12192136ddb689465d9e0ab67b6774d30
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-12-17 00:54:53 +0200

    Create ShopController

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/controller/ShopController.java
@@ -0,0 +1,33 @@
+package edu.alexu.cse.dripmeup.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.alexu.cse.dripmeup.component.ShopManager;
+import edu.alexu.cse.dripmeup.dto.ProductSnapshot;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/1000/shop")
+public class ShopController {
+    
+    @Autowired
+    private ShopManager shopManager;
+    
+    @GetMapping("/products")
+    public ResponseEntity<Page<ProductSnapshot>> getProducts(@RequestParam("page") int page) {
+        Page<ProductSnapshot> products = shopManager.getAllProducts(page, 10);
+        if ( products == null || products.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(null);
+    }
+
+}
+
