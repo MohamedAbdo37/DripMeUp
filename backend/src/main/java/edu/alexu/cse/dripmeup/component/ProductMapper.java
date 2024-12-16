0ddd2f3d91141d9eadbe48ba4dceb796f84a0d53
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-12-17 01:48:51 +0200

    add getProduct API

--- backend/src/main/java/edu/alexu/cse/dripmeup/component/ProductMapper.java
+++ backend/src/main/java/edu/alexu/cse/dripmeup/component/ProductMapper.java
@@ -3,11 +3,17 @@ package edu.alexu.cse.dripmeup.component;
 import org.springframework.stereotype.Component;
 
 import edu.alexu.cse.dripmeup.dto.ProductSnapshot;
+import edu.alexu.cse.dripmeup.dto.Variant;
 import edu.alexu.cse.dripmeup.entity.product.ProductEntity;
+import edu.alexu.cse.dripmeup.entity.product.VariantEntity;
 
 @Component
 public class ProductMapper {
-    public ProductSnapshot toDTO(ProductEntity productEntity) {
+    public ProductSnapshot toPSDTO(ProductEntity productEntity) {
         return new ProductSnapshot(productEntity);
+    }
+
+    public Variant toVariantDTO(VariantEntity variantEntity) {
+        return new Variant(variantEntity);
     }
 }
--- backend/src/main/java/edu/alexu/cse/dripmeup/component/ShopManager.java
+++ backend/src/main/java/edu/alexu/cse/dripmeup/component/ShopManager.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import edu.alexu.cse.dripmeup.dto.Product;
 import edu.alexu.cse.dripmeup.dto.ProductSnapshot;
+import edu.alexu.cse.dripmeup.dto.Variant;
 import edu.alexu.cse.dripmeup.entity.product.ProductEntity;
+import edu.alexu.cse.dripmeup.entity.product.VariantEntity;
 import edu.alexu.cse.dripmeup.repository.ItemRepository;
 import edu.alexu.cse.dripmeup.repository.ProductRepository;
 import edu.alexu.cse.dripmeup.repository.VariantRepository;
@@ -43,10 +46,19 @@ public class ShopManager {
 
     public Page<ProductSnapshot> getAllProducts(int page, int size) {
         Page<ProductEntity> products = new ProductService().getAllProducts(this.productRepository, page, size);
-        return products.map(this.productMapper::toDTO);
+        return products.map(this.productMapper::toPSDTO);
+    }
+
+    public List<Variant> getVariantsOfProduct(ProductEntity product) {
+        List<VariantEntity> variants = new ProductService().getVariantsOfProduct(product);
+        return variants.stream().map(this.productMapper::toVariantDTO).toList();
     }
 
     public String getImageOfProduct(ProductEntity product) {
         return new ProductService().getImageOfProduct(product, this.variantRepository);
+    }
+
+    public Product getProduct(long productID) {
+        return new Product(new ProductService().getProduct(this.productRepository, productID));
     }
 }
