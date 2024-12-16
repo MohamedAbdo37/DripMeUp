29082751bb1aeb3136633e26b40331769332d002
Mohamed Abdel-Moneim <mabdelmoneim737@gmail.com>
2024-12-17 00:52:19 +0200

    add Repositories and ProductState enum

---
+++ backend/src/main/java/edu/alexu/cse/dripmeup/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package edu.alexu.cse.dripmeup.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.alexu.cse.dripmeup.entity.product.ProductEntity;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    @SuppressWarnings("null")
+    @Override
+    Page<ProductEntity> findAll(Pageable pageable);
+}
