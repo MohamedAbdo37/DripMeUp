61c62e1458b8dc1d2e5f076b238b1fd119471e07
zoaa3054 <zoaa3054@gmail.com>
2025-01-01 00:40:37 +0200

    .

--- frontend/src/Components/ObjectToAppear.jsx
+++ frontend/src/Components/ObjectToAppear.jsx
@@ -1,7 +1,7 @@
 import { ClipLoader } from "react-spinners";
 const ObjectToAppear = ({ size }) => {
   return (
-    <ClipLoader size={size} color="white" />
+    <ClipLoader size={size} color="black" />
   );
 };
 export default ObjectToAppear;
